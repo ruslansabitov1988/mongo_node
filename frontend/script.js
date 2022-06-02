dd775b5764fd5f5a89b0f651cb0bc68eb718515f
Сабитов Руслан Сабитович <@lcoast.itstep>
2022-06-02 16:42:39 +0600

    FINAL

--- frontend/script.js
+++ frontend/script.js
@@ -47,7 +47,7 @@ async function drawUsers(){
     for(let item of usersArray){
         usersBlock.append(
             `
-                <div class="item" onclick="selectUser('${item._id}')">
+                <div class="user_item" onclick="selectUser('${item._id}')">
                     <p> Full name: ${item.fullName}</p>
                 </div>
             `
@@ -91,7 +91,7 @@ async function drawCars(){
     for(let item of carsArray){
         carsBlock.append(
             `
-                <div class='item' onclick='selectCar("${item._id}")'>                                       
+                <div class='car_item' onclick='selectCar("${item._id}")'>                                       
                     <p> Model: ${item.model}</p>
                     <p> Color: ${item.color}</p>
                     <p> Year: ${item.year}</p>
@@ -106,14 +106,26 @@ async function drawCars(){
 drawUsers().then(()=>{
 
     drawCars().then(()=>{
-        let userItems = $(".item");
+        let userItems = $(".user_item");
+        let carItems = $(".car_item");
         
-        console.log(userItems.length)
+        
        
-
         for( let item of userItems){
             item.addEventListener ( "click",()=>{
-                item.classList.toggle("selected")
+                for(let user of userItems){
+                    user.classList.remove("selected")
+                }
+                item.classList.add("selected")
+            })
+        }
+
+        for( let item of carItems){
+            item.addEventListener ( "click",()=>{
+                for(let car of carItems){
+                    car.classList.remove("selected")
+                }
+                item.classList.add("selected")
             })
         }
 
